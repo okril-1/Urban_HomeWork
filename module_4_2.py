ed40064b96d6a823ccff3e1cc50f0007d004e7a7
Denis <dtaldykin@inbox.ru>
2024-10-26 15:01:42 +0300

    14/10-2

---
+++ module_4_2.py
@@ -0,0 +1,15 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    # Вызовем inner_function внутри test_function
+    inner_function()
+
+# Вызовем test_function
+test_function()
+
+# Попробуем вызвать inner_function вне test_function
+try:
+    inner_function()
+except NameError as e:
+    print(f"Ошибка: {e}")
